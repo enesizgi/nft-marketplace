238446a7e3c61c30106c69fea322e8868524a95d
MUHAMMED ENES IZGI <e2310142@ceng.metu.edu.tr>
2022-11-07 21:07:40 +0300

    provide a simple script to deploy contracts

---
+++ src/backend/scripts/deploy.js
@@ -0,0 +1,36 @@
+const main = async () => {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const NFT = await ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy();
+
+  console.log("NFT contract address:", nft.address);
+
+  saveFrontendFiles(nft, "NFT");
+}
+
+const saveFrontendFiles = (contract, name) => {
+  const fs = require('fs');
+  const contractsDir = __dirname + "/../../frontend/contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
+
+}
+
+main()
