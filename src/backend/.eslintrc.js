6193d68e337acdadfc6068794b35b6b64fb3d115
MUHAMMED ENES IZGI <e2310142@ceng.metu.edu.tr>
2022-11-21 00:32:50 +0300

    chore(backend): create eslint config for backend, disable some rules

---
+++ src/backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 0
+  }
+};
