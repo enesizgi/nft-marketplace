3a21b655d39d2cee52f8ec5f266a81c14aca79bc
onclmrl <e2310324@ceng.metu.edu.tr>
2022-11-29 00:45:55 +0300

    feat(frontend): profile picture upload/remove functionality added

--- src/frontend/components/Profile/ProfileHeader.js
+++ src/frontend/components/Profile/ProfileHeader.js
@@ -4,7 +4,7 @@ import API from '../../modules/api';
 import ScProfileHeader from './ScProfileHeader';
 
 const ProfileHeader = ({ id, isOwner }) => { // eslint-disable-line no-unused-vars
-  const [profilePhoto, setProfilePhoto] = useState('');
+  const [profilePhoto, setProfilePhoto] = useState(''); // eslint-disable-line no-unused-vars
   const [coverPhoto, setCoverPhoto] = useState('');
   const [username, setUsername] = useState('');
 
@@ -14,11 +14,35 @@ const ProfileHeader = ({ id, isOwner }) => { // eslint-disable-line no-unused-va
     API.getUsername(id).then(response => setUsername(response.name));
   });
 
+  const [selectedImage, setSelectedImage] = useState(null);
+
+
   return (
     <ScProfileHeader>
       <img className="cover-photo" src={coverPhoto} alt="coverPhoto" />
+
+
       <div className="profile-info">
-        <img className="profile-photo" src={profilePhoto} alt="profilePhoto" />
+        <div>
+          {selectedImage && (
+              <div>
+                <img className="profile-photo" alt="profilePhoto"  src={URL.createObjectURL(selectedImage)} />
+                <br />
+                <button type="button" onClick={()=>setSelectedImage(null)}>Remove</button>
+              </div>
+          )}
+          <br />
+
+          <br />
+          <input
+              type="file"
+              name="myImage"
+              onChange={(event) => {
+                console.log(event.target.files[0]);
+                setSelectedImage(event.target.files[0]);
+              }}
+          />
+        </div>
         <div className="profile-names">
           { username && <h1 className="profile-names-name">{username}</h1> }
           <h2 className="profile-names-id">{id}</h2>
@@ -37,5 +61,7 @@ ProfileHeader.defaultProps = {
   id: '',
   isOwner: false
 };
+
+
 
 export default ProfileHeader;
