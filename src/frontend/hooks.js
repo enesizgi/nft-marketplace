9a248897292ab1cab0f8e907c3c902e556928a29
MUHAMMED ENES IZGI <e2310142@ceng.metu.edu.tr>
2022-12-27 20:15:12 +0300

    fix: modal can take 0 arguments

--- src/frontend/hooks.js
+++ src/frontend/hooks.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 // eslint-disable-next-line import/prefer-default-export
-export const useClickOutsideAlert = (ref, callback, args) => {
+export const useClickOutsideAlert = (ref, callback, args = []) => {
   useEffect(() => {
     const handleClickOutside = e => {
       if (ref.current && !ref.current.contains(e.target)) {
