3cf62d84c49580aaf0aea086a8ed6d843356e9d2
MUHAMMED ENES IZGI <e2310142@ceng.metu.edu.tr>
2023-01-04 18:46:04 +0300

    feat(frontend): get nft info in groups of 5

--- src/frontend/components/ListNFTS.js
+++ src/frontend/components/ListNFTS.js
@@ -8,69 +8,66 @@ import LoadingSpinner from './LoadingSpinner';
 const ListNFTSPage = ({ profileId, selectedTab }) => {
   const [loading, setLoading] = useState(true);
   const [listedItems, setListedItems] = useState([]);
+  /* eslint-disable no-unused-vars */
+  const [listedCurrentPage, setListedCurrentPage] = useState(1);
+  const [auctionCurrentPage, setAuctionCurrentPage] = useState(1);
+  /* eslint-enable no-unused-vars */
   const marketplaceContract = useSelector(getMarketplaceContract);
   const nftContract = useSelector(getNFTContract);
-  const loadAuctionItems = async () => {
-    const itemCount = await marketplaceContract.auctionItemCount();
-    // const auctionItems = [];
-    const auctionItemsIds = [];
-    // Create an array of all auction item ids starts from 1 to auctionItemCount
-    for (let i = 1; i <= itemCount; i += 1) {
-      auctionItemsIds.push(i);
-    }
 
-    const auctionItems = await Promise.allSettled(
-      auctionItemsIds.map(async indx => {
-        const i = await marketplaceContract.auctionItems(indx);
-        if (i.claimed) return null;
-        if (selectedTab !== 'Home' && i.seller.toLowerCase() !== profileId) return null;
-        // get uri url from nft contract
-        const uri = await nftContract.tokenURI(i.tokenId);
-        const cid = uri.split('ipfs://')[1];
-        // use uri to fetch the nft metadata stored on ipfs
-        const metadata = await API.getFromIPFS(cid);
-        // define listed item object
-        return {
-          ...i,
-          ...metadata
-        };
-      })
-    );
-    return auctionItems.filter(i => i.status === 'fulfilled' && i.value != null).map(i => i.value);
-  };
-  const loadListedItems = async () => {
-    // Load all sold items that the user listed
-    const itemCount = await marketplaceContract.itemCount();
-    const offeredItemIds = [];
-    // TODO @Enes: Rename above two variables again
-    for (let i = 1; i <= itemCount; i += 1) {
-      offeredItemIds.push(i);
+  const getItems = async isAuctionItems => {
+    const itemCount = isAuctionItems ? await marketplaceContract.auctionItemCount() : await marketplaceContract.itemCount();
+    const currentPage = isAuctionItems ? auctionCurrentPage : listedCurrentPage;
+    const items = [];
+    for (let cursor = 0; itemCount - (currentPage - 1 + cursor) * 5 >= 1; cursor += 1) {
+      const itemsBegin = Math.max(1, itemCount - (currentPage - 1 + cursor) * 5);
+      const itemsEnd = Math.max(itemsBegin - 5, 0);
+      const itemIds = [];
+      for (let i = itemsBegin; i > itemsEnd; i -= 1) {
+        itemIds.push(i);
+      }
+      // eslint-disable-next-line no-await-in-loop
+      const auctionItems = await Promise.allSettled(
+        itemIds.map(async indx => {
+          const i = isAuctionItems ? await marketplaceContract.auctionItems(indx) : await marketplaceContract.items(indx);
+          const soldCriteria = isAuctionItems ? i.claimed : i.sold;
+          if (soldCriteria) return null;
+          if (selectedTab !== 'Home' && i.seller.toLowerCase() !== profileId) return null;
+          // get uri url from nft contract
+          const uri = await nftContract.tokenURI(i.tokenId);
+          const cid = uri.split('ipfs://')[1];
+          // use uri to fetch the nft metadata stored on ipfs
+          const metadata = await API.getFromIPFS(cid);
+          // define listed item object
+          if (isAuctionItems) {
+            return {
+              ...i,
+              ...metadata
+            };
+          }
+          const totalPrice = await marketplaceContract.getTotalPrice(i.itemId);
+          // define listed item object
+          return {
+            ...i,
+            ...metadata,
+            totalPrice,
+            price: i.price,
+            itemId: i.itemId
+          };
+        })
+      );
+      const batchItems = auctionItems.filter(i => i.status === 'fulfilled' && i.value != null).map(i => i.value);
+      items.push(...batchItems);
+      if (items.length === 5) return items;
+      if (items.length > 5) {
+        items.length = 5;
+        return items;
+      }
     }
-
-    const offeredItems = await Promise.allSettled(
-      offeredItemIds.map(async indx => {
-        const i = await marketplaceContract.items(indx);
-        if (i.sold) return null;
-        if (selectedTab !== 'Home' && i.seller.toLowerCase() !== profileId) return null;
-        // get uri url from nft contract
-        const uri = await nftContract.tokenURI(i.tokenId);
-        const cid = uri.split('ipfs://')[1];
-        // use uri to fetch the nft metadata stored on ipfs
-        const metadata = await API.getFromIPFS(cid);
-        // get total price of item (item price + fee)
-        const totalPrice = await marketplaceContract.getTotalPrice(i.itemId);
-        // define listed item object
-        return {
-          ...i,
-          ...metadata,
-          totalPrice,
-          price: i.price,
-          itemId: i.itemId
-        };
-      })
-    );
-    return offeredItems.filter(i => i.status === 'fulfilled' && i.value != null).map(i => i.value);
+    return items;
   };
+  const loadAuctionItems = async () => getItems(true);
+  const loadListedItems = async () => getItems(false);
 
   useEffect(async () => {
     const items = await Promise.allSettled([loadListedItems(), loadAuctionItems()]);
