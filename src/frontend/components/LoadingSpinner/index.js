6de33863859ca16a470cc57fee81466fd13a7ae5
e23805902 <e2380590@ceng.metu.edu.tr>
2023-01-10 16:58:36 +0300

    fix(frontend): center loading spinner and remove unnecessary css

--- src/frontend/components/LoadingSpinner/index.js
+++ src/frontend/components/LoadingSpinner/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import './LoadingSpinner.css';
 import Styled from 'styled-components';
 
-const ScPurchasesPage = Styled.div`
+const ScLoadingSpinner = Styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    width: 100%;
+    height: 100%;
+ 
+    .loading-spinner {
+      border: 8px solid rgba(0, 137, 168, 0.3);
+      border-top: 8px solid var(--blue);
+      border-radius: 50%;
+      width: 60px;
+      height: 60px;
+      animation: spin 0.8s linear infinite;
+    }
+    
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
 `;
 
 const LoadingSpinner = () => (
-  <ScPurchasesPage>
+  <ScLoadingSpinner>
     <div className="loading-spinner" />
-  </ScPurchasesPage>
+  </ScLoadingSpinner>
 );
 
 export default LoadingSpinner;
