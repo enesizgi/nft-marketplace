0ae7b9d7f2c667899f7b6ab18bcc742807af9323
MUHAMMED ENES IZGI <e2310142@ceng.metu.edu.tr>
2023-03-14 21:10:13 +0300

    chore: remove mysql related files

--- src/backend/migrations/1669043767225_create_table_users_seeds.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // You can add mock data to here
-  up: `INSERT IGNORE INTO user (walletId, slug, name)
-VALUES ('temp_wallet_id', 'temp_slug', 'Emir-test');`,
-  down: 'DELETE FROM user WHERE id = 1;'
-};
--- src/backend/migrations/1672158222081_create_table_transaction_seed.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  up: `INSERT IGNORE INTO transaction(price, event, sender, owner, contract_id, block_number)
-        values (100, 'mint', '0xca502b9dff7c0455cb39df1c9de80d0485179120', '0x2546bcd3c84621e976d8185a91a922ae77ecec30', 'sample_contract_id', 1);`,
-  down: "DELETE FROM transaction WHERE contract_id = 'sample_contract_id';"
-};
