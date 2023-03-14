c943e3aea5d0a5213ad6ff8455bb3b146d76c53f
MUHAMMED ENES IZGI <e2310142@ceng.metu.edu.tr>
2023-03-14 21:47:22 +0300

    feat(backend): add nft status mongodb model

---
+++ src/backend/models/nft_status.js
@@ -0,0 +1,25 @@
+import * as mongoose from 'mongoose';
+
+const NftStatusSchema = new mongoose.Schema(
+  {
+    type: Number,
+    itemId: Number,
+    nft: String,
+    price: String,
+    tokenId: Number,
+    seller: String,
+    buyer: String,
+    claimed: Boolean,
+    canceled: Boolean,
+    sold: Boolean,
+    timeToEnd: Date
+  },
+  {
+    typeKey: '$type',
+    timestamps: { createdAt: 'doc_created_at', updatedAt: 'doc_updated_at' }
+  }
+);
+
+const NftStatus = mongoose.model('NftStatus', NftStatusSchema, 'nft_status');
+
+export default NftStatus;
