256405f2c07ca1f655bde760b9e6189ca2b5608a
e23805902 <e2380590@ceng.metu.edu.tr>
2023-03-29 14:28:44 +0300

    feat(frontend): validation for profile edit modal

--- apps/frontend/src/frontend/components/Profile/ProfileEditModal.jsx
+++ apps/frontend/src/frontend/components/Profile/ProfileEditModal.jsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import { Input, FormControl, FormLabel, Button } from '@chakra-ui/react';
+import { Input, FormControl, FormLabel, Button, FormHelperText } from '@chakra-ui/react';
+import isEqual from 'lodash/isEqual';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDeviceType, getSignedMessage } from '../../store/selectors';
 import { DEVICE_TYPES } from '../../constants';
@@ -30,6 +31,10 @@ const ScProfileEditModal = styled(FormControl)`
   button {
     word-wrap: break-word;
     max-width: calc(50% - 20px);
+  }
+  .chakra-form__helper-text {
+    transform: translateY(-20px);
+    color: red;
   }
   .submitButton {
     margin-top: 20px;
@@ -73,32 +78,75 @@ const ScProfileEditModal = styled(FormControl)`
 
 const ProfileEditModal = ({ profile, updateSignedMessage }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isChanged, setIsChanged] = useState(false);
   const [currentProfile, setCurrentProfile] = useState(profile);
   const [uploadedCoverPhoto, setUploadedCoverPhoto] = useState();
   const [uploadedProfilePhoto, setUploadedProfilePhoto] = useState();
+  const [errorMessages, setErrorMessages] = useState({});
   const profilePhotoUploadRef = useRef();
   const coverPhotoUploadRef = useRef();
   const dispatch = useDispatch();
   const deviceType = useSelector(getDeviceType);
   const signedMessage = useSelector(getSignedMessage);
+
+  useEffect(() => {
+    setIsChanged(!isEqual(profile, currentProfile));
+  }, [currentProfile, profile]);
+
   const handleOpenProfilePhotoUpload = () => {
     profilePhotoUploadRef.current?.click();
   };
+
   const handeOpenCoverPhotoUpload = () => {
     coverPhotoUploadRef.current?.click();
   };
-  const handleProfilePhotoUpload = async e => {
+
+  const handleProfilePhotoUpload = e => {
     const file = e.target.files[0];
-    setUploadedProfilePhoto(file);
-    const url = URL.createObjectURL(file);
-    setCurrentProfile({ ...profile, profilePhoto: url });
+    if (file) {
+      setUploadedProfilePhoto(file);
+      const url = URL.createObjectURL(file);
+      setCurrentProfile({ ...currentProfile, profilePhoto: url });
+      setErrorMessages({ ...errorMessages, profile: '' });
+    } else {
+      setErrorMessages({ ...errorMessages, profile: 'Please upload a valid profile photo.' });
+    }
   };
 
-  const handleCoverPhotoUpload = async e => {
+  const handleCoverPhotoUpload = e => {
     const file = e.target.files[0];
-    setUploadedCoverPhoto(file);
-    const url = URL.createObjectURL(file);
-    setCurrentProfile({ ...profile, coverPhoto: url });
+    if (file) {
+      setUploadedCoverPhoto(file);
+      const url = URL.createObjectURL(file);
+      setCurrentProfile({ ...currentProfile, coverPhoto: url });
+      setErrorMessages({ ...errorMessages, cover: '' });
+    } else {
+      setErrorMessages({ ...errorMessages, cover: 'Please upload a valid cover photo.' });
+    }
+  };
+
+  const validateName = e => {
+    if (!e.target.value) {
+      setCurrentProfile({ ...currentProfile, name: profile.name });
+      setErrorMessages({ ...errorMessages, name: 'Account name cannot be empty' });
+    } else {
+      setCurrentProfile({ ...currentProfile, name: e.target.value });
+      setErrorMessages({ ...errorMessages, name: '' });
+    }
+  };
+
+  const validateSlug = async e => {
+    if (e.target.value) {
+      const result = await API.getUserBySlug(currentProfile.slug);
+      if (result && result.id !== currentProfile.id) {
+        setErrorMessages({ ...errorMessages, slug: 'This slug is being used by another user.' });
+      } else {
+        setCurrentProfile({ ...currentProfile, slug: e.target.value });
+        setErrorMessages({ ...errorMessages, slug: '' });
+      }
+    } else {
+      setCurrentProfile({ ...currentProfile, slug: null });
+    }
   };
 
   const handleSubmitData = async () => {
@@ -123,37 +171,54 @@ const ProfileEditModal = ({ profile, updateSignedMessage }) => {
 
   return (
     <ScProfileEditModal>
-      <FormLabel>Account Name</FormLabel>
+      <FormLabel htmlFor="name">Account Name</FormLabel>
       <Input
         type="text"
+        id="name"
         value={currentProfile.name}
-        onChange={e => setCurrentProfile({ ...profile, name: e.target.value })}
+        isInvalid={!!errorMessages.name}
+        onChange={e => setCurrentProfile({ ...currentProfile, name: e.target.value })}
+        onBlur={validateName}
         onClick={e => e.target.setSelectionRange(0, e.target.value.length)}
       />
-      <FormLabel>Profile Slug</FormLabel>
+      <FormHelperText>{errorMessages.name}</FormHelperText>
+      <FormLabel htmlFor="slug">Profile Slug</FormLabel>
       <Input
         type="text"
+        id="slug"
         value={currentProfile.slug}
-        onChange={e => setCurrentProfile({ ...profile, name: e.target.value })}
+        isInvalid={!!errorMessages.slug}
+        onChange={e => setCurrentProfile({ ...currentProfile, slug: e.target.value })}
+        onBlur={validateSlug}
         onClick={e => e.target.setSelectionRange(0, e.target.value.length)}
       />
-      <FormLabel>Profile Photo</FormLabel>
+      <FormHelperText>{errorMessages.slug}</FormHelperText>
+      <FormLabel htmlFor="profilePhoto">Profile Photo</FormLabel>
       <div className="photoUpload-container">
-        <img className="profilePhoto" alt="profilePhoto" src={currentProfile.profilePhoto} />
+        <img className="profilePhoto" alt="profilePhoto" id="profilePhoto" src={currentProfile.profilePhoto} />
         <Button size={BUTTON_SIZE_MAP[deviceType]} colorScheme="linkedin" onClick={handleOpenProfilePhotoUpload}>
           Change Profile Photo
         </Button>
-        <input ref={profilePhotoUploadRef} type="file" onChange={handleProfilePhotoUpload} style={{ display: 'none' }} />
+        <input ref={profilePhotoUploadRef} type="file" accept="image/*" onChange={handleProfilePhotoUpload} style={{ display: 'none' }} />
+        <FormHelperText>{errorMessages.profile}</FormHelperText>
       </div>
-      <FormLabel>Cover Photo</FormLabel>
+      <FormLabel htmlFor="coverPhoto">Cover Photo</FormLabel>
       <div className="photoUpload-container">
-        <img className="coverPhoto" alt="coverPhoto" src={currentProfile.coverPhoto} />
+        <img className="coverPhoto" alt="coverPhoto" id="coverPhoto" src={currentProfile.coverPhoto} />
         <Button size={BUTTON_SIZE_MAP[deviceType]} colorScheme="linkedin" onClick={handeOpenCoverPhotoUpload}>
           Change Cover Photo
         </Button>
-        <input ref={coverPhotoUploadRef} type="file" onChange={handleCoverPhotoUpload} style={{ display: 'none' }} />
+        <input ref={coverPhotoUploadRef} type="file" accept="image/*" onChange={handleCoverPhotoUpload} style={{ display: 'none' }} />
+        <FormHelperText>{errorMessages.cover}</FormHelperText>
       </div>
-      <Button colorScheme="linkedin" className="submitButton" size={BUTTON_SIZE_MAP[deviceType]} type="submit" onClick={handleSubmitData}>
+      <Button
+        isDisabled={!isChanged}
+        colorScheme="linkedin"
+        className="submitButton"
+        size={BUTTON_SIZE_MAP[deviceType]}
+        type="submit"
+        onClick={handleSubmitData}
+      >
         Save Changes
       </Button>
     </ScProfileEditModal>
