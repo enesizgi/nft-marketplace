29fc6e7b48916b1f820b5ef07997fe6616b8307b
MUHAMMED ENES IZGI <e2310142@ceng.metu.edu.tr>
2023-06-06 05:49:03 +0300

    chore: remove use-swipe-hook

--- apps/frontend/src/frontend/components/NFTSlider/index.jsx
+++ apps/frontend/src/frontend/components/NFTSlider/index.jsx
@@ -1,20 +1,13 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { TfiArrowCircleLeft, TfiArrowCircleRight } from 'react-icons/tfi';
-import { useSwipe } from 'use-swipe-hook';
 import NFTShowcase from '../NFTShowcase';
 
 const NFTSlider = ({ itemCount, selectedTab, currentPage, setCurrentPage, loading, items, onePageNftCount }) => {
   const nftSliderRef = useRef();
-  const swipeDirection = useSwipe({ ref: nftSliderRef, thresholdPX: 5 });
   const placeHolderItems = [{}, {}, {}, {}, {}];
   const pages = [...Array(Math.ceil(itemCount / onePageNftCount) + 1).keys()].slice(1);
   const onRightHandler = () => setCurrentPage(prev => Math.min(Math.ceil(itemCount / onePageNftCount), prev + 1));
   const onLeftHandler = () => setCurrentPage(prev => Math.max(1, prev - 1));
-
-  useEffect(() => {
-    if (swipeDirection === 'left') onRightHandler();
-    if (swipeDirection === 'right') onLeftHandler();
-  }, [swipeDirection]);
 
   if (itemCount === 0) return null;
 
